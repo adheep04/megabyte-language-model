25fc3746cc7c92ff03a52d923eb8a3237fd593a5
Adheep Rampure <adheep@gmail.com>
2025-02-03 21:49:10 -0500

    2

---
+++ model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+
+class MegabyteLanguageModel(nn.Module):
+    def __init__(self,):
+        return
+    
+    def forward(self, x):
+       return 
+
+class PatchEmbedding(nn.Module):
+    
